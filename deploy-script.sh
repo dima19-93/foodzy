6a4a030fb95bfb6e76d15d7f80260e723620e08c
dima19-93 <dima70593@ukr.net>
2025-01-27 23:27:09 +0200

    Create deploy-script.sh
    add file

---
+++ deploy-script.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+docker stop new-apples || true
+docker rm new-apples || true
+
+docker pull ${{ secrets.DOCKER_USERNAME }}/foodzy:latest
+
+docker run -d --name new-apples -p 80:80 -p 443:443 -v /diplom_work/ssl:/etc/ssl -v /diplom_work/ssl/fullchain.pem:/etc/ssl/certs/fullchain.pem ${{ secrets.DOCKER_USERNAME }}/foodzy:latest
