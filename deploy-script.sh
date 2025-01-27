6c618bf8068194e4f81e60007cefa101cad96cc8
dima19-93 <dima70593@ukr.net>
2025-01-27 23:55:18 +0200

    Update deploy-script.sh
    sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-docker stop new-apples || true
-docker rm new-apples || true
+# Логин в DockerHub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker pull ${{ secrets.DOCKER_USERNAME }}/foodzy:latest
+# Сборка и пуш Docker-образа
+docker build -t $DOCKER_USERNAME/foodzy:latest .
+docker push $DOCKER_USERNAME/foodzy:latest
 
-docker run -d \
---name new-apples \ 
--p 80:80 \ 
--p 443:443 \ 
--v /diplom_work/ssl:/etc/ssl -v /diplom_work/ssl/fullchain.pem:/etc/ssl/certs/fullchain.pem 
-${{ secrets.DOCKER_USERNAME }}/foodzy:latest
+# Деплой на сервер
+ssh -tt -o StrictHostKeyChecking=no -i id_rsa adminserver@$SERVER_IP << 'EOF'
+  docker pull $DOCKER_USERNAME/foodzy:latest
+  docker stop new-apples || true
+  docker rm new-apples || true
+  docker run -d --name new-apples -p 80:80 -p 443:443 -v /diplom_work/ssl:/etc/ssl -v /diplom_work/ssl/fullchain.pem:/etc/ssl/certs/fullchain.pem $DOCKER_USERNAME/foodzy:latest
+EOF
