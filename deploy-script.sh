846aa3f34cdf2a45dca571b611a1c0e1633482ed
dima19-93 <dima70593@ukr.net>
2025-01-27 23:47:41 +0200

    Update deploy-script.sh
    запуск sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -5,4 +5,9 @@ docker rm new-apples || true
 
 docker pull ${{ secrets.DOCKER_USERNAME }}/foodzy:latest
 
-docker run -d --name new-apples -p 80:80 -p 443:443 -v /diplom_work/ssl:/etc/ssl -v /diplom_work/ssl/fullchain.pem:/etc/ssl/certs/fullchain.pem ${{ secrets.DOCKER_USERNAME }}/foodzy:latest
+docker run -d \
+--name new-apples \ 
+-p 80:80 \ 
+-p 443:443 \ 
+-v /diplom_work/ssl:/etc/ssl -v /diplom_work/ssl/fullchain.pem:/etc/ssl/certs/fullchain.pem 
+${{ secrets.DOCKER_USERNAME }}/foodzy:latest
