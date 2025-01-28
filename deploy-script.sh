758a00a703fda5270cb82d6b6c79dccc0dce6bd5
dima19-93 <dima70593@ukr.net>
2025-01-28 16:45:49 +0200

    Update deploy-script.sh
    исправление

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,5 +1,16 @@
+cd /home/adminserver/diplom 
+echo "Using Docker username: $DOCKER_USERNAME" 
+docker pull ${{ secrets.DOCKER_USERNAME }}/foodzy:latest
 
-
+docker stop new-apples || true
+docker rm new-apples || true
  
+docker run -d \
+  --name new-apples \
+  -p 80:80 \
+  -p 443:443 \
+  -v /diplom_work/ssl:/etc/ssl -v /diplom_work/ssl/fullchain.pem:/etc/ssl/certs/fullchain.pem ${{ secrets.DOCKER_USERNAME }}/foodzy:latest
+        EOF
+      
  
  
